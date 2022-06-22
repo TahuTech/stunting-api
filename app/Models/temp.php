60ef7ee3d6a67ac3e2bd0da68013c4c1943432fe
TahuTech <18053036@student.uwp.ac.id>
2022-06-22 09:59:01 +0700

    temp model

---
+++ app/Models/temp.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class temp extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['umur', 'berat', 'tinggi', 'lkkepala', 'gizi', 'berat', 'tinggi', 'stunting'];
+}
---
+++ database/migrations/2022_06_22_025348_create_temps_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTempsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('temps', function (Blueprint $table) {
+            $table->id();
+            $table->double('umur');
+            $table->double('berat');
+            $table->double('tinggi');
+            $table->double('lkkepala');
+            $table->enum('gizi', ['1', '2', '3', '4']);
+            $table->enum('berat', ['1', '2', '3', '4']);
+            $table->enum('tinggi', ['1', '2', '3', '4']);
+            $table->enum('stunting', ['1', '2', '3', '4']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('temps');
+    }
+}
